e7c2f1b372a391c54b8aa64cd6719aa0b7be23aa
muhammaddariazzidane <dariazzidane@gmail.com>
2024-04-18 05:23:26 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+app.use(
+  cors({
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Origin',
+      'x-access-token',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+  })
+);
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Listening: http://localhost:${port}`);
+});
+
+export default app;
